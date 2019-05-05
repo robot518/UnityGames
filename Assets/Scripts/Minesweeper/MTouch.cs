c39d31ff192d7b948107db60c38951a4cda48ea6
SmileChen518 <262000730@qq.com>
2019-05-05 23:59:48 +0800

    tilemap to img

---
+++ Assets/Scripts/Minesweeper/MTouch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MTouch : MonoBehaviour, IPointerClickHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //Debug.Log(eventData);
+        var pos = eventData.position;
+        var iLen = 60;
+        if (pos.x >= 90 && pos.x < 630 && pos.y >= 380 && pos.y < 920)
+        {
+            var col = Mathf.Floor((pos.x - 90) / iLen);
+            var row = 8 - Mathf.Floor((pos.y - 380) / iLen);
+            Debug.Log(col + "," + row);
+        }
+    }
+}
